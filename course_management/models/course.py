9c06ce0fdb07358b652a69a9c04697848ec147eb
Ser-D <ipc.avanti@gmail.com>
2024-05-12 21:21:43 +0300

    Work+

--- course_management/models/course.py
+++ course_management/models/course.py
@@ -9,7 +9,7 @@ class Course(models.Model):
     description = fields.Text(string='Description')
     teacher_id = fields.Many2one(comodel_name='res.partner', string='Teacher', required=True)
     start_date = fields.Date(string='Start Date', required=True)
-    duration = fields.Integer(string='Duration (days or hours)', required=True)
+    duration = fields.Integer(string='Duration days', required=True)
     participant_ids = fields.One2many(comodel_name='participant', inverse_name='course_id')
     lesson_ids = fields.One2many(comodel_name='lesson', inverse_name='course_id')
 
