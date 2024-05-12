9732e618823b6a792789f0760d40109c6ce14c57
Ser-D <ipc.avanti@gmail.com>
2024-05-12 21:29:09 +0300

    Work++

--- course_management/__manifest__.py
+++ course_management/__manifest__.py
@@ -4,10 +4,10 @@
 
 {
     'name': 'Course management',
-    'category': 'Sales/CRM',
+    'category': 'Study/CRM',
     'sequence': 150,
-    'summary': 'Centralize your address book',
-    'description': """
+    'summary': 'i am python developer',
+    'description': """   I am BEST!!!!!
 This module gives you a quick view of your contacts directory, accessible from your home page.
 You can track your vendors, customers and other contacts.
 """,
